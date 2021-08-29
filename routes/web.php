a9c7ae4ff1f0cc86b2a695c7764364bc33adf25d
qiuxan <qiuxan@gmail.com>
2021-08-29 16:00:06 +1000

    without commetn

--- routes/web.php
+++ routes/web.php
@@ -21,9 +21,7 @@ Route::get('/posts/{post}', function ($slug) {
 
 
 Route::get('post',function(){
-//    return view('post',[
-//        'post'=>'<h1>hello world<h1>'// in the post view now $post is accessible
-//    ]);
+
 
     return ['foo'=>'Bar'];
 });
@@ -31,32 +29,9 @@ Route::get('test',function(){
     return view('test');
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 Route::get('/',function(){
 
     $files=File::files(resource_path('/posts'));
-//    ddd($files[0]->getFilenameWithoutExtension());
-    $posts=[];
 
     $posts=collect($files)->map(function($file){
         $document=YamlFrontMatter::parseFile($file);
@@ -67,53 +42,7 @@ Route::get('/',function(){
             $document->body(),
             $file->getFilenameWithoutExtension());
     });
-
-//    $posts=array_map(function ($file){
-//        $document=YamlFrontMatter::parseFile($file);
-//        return new Post(
-//            $document->title,
-//            $document->excerpt,
-//            $document->date,
-//            $document->body(),
-//            $file->getFilenameWithoutExtension()
-//        );
-//    },$files);
-//    $documents=[];
-
-//    foreach ($files as $file){
-////        $documents[]=YamlFrontMatter::parseFile($file);
-//        $document=YamlFrontMatter::parseFile($file);
-//
-////        ddd($document);
-////        dd($document->title);
-//        $posts[]=new Post(
-//            $document->title,
-//            $document->excerpt,
-//            $document->date,
-//            $document->body(),
-//            $file->getFilenameWithoutExtension()
-//        );
-//    }
-
-
-
-//    ddd($posts[0]->slug);
-
-
-
-
-//    $document= YamlFrontMatter::parseFile(resource_path('/posts/my-first-post.html'));
-
-//    ddd($document->matter('title'));
-
-
-
-
-
-
-
-//    $posts=Post::all();
-//
+    
     return view('posts',[
         'posts'=> $posts
     ]);
